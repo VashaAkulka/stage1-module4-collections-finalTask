bf7b29983dcbfa2d5544d4613d628bb2630f856d
Виктор <eee@mail.ru>
2024-01-08 21:24:14 +0300

    yes

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,32 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collector;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+        Set<Map.Entry<String, Set<String>>> entrySet = projects.entrySet();
+
+        for (Map.Entry<String, Set<String>> entry : entrySet) {
+            Set<String> set = entry.getValue();
+            if (set.contains(developer)) {
+                list.add(entry.getKey());
+            }
+        }
+
+        list.sort(new StringComparator());
+        return list;
+    }
+}
+
+class StringComparator implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        if (o1.length() == o2.length()) {
+            return o2.compareTo(o1);
+        } else {
+            return o2.length() - o1.length();
+        }
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,26 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+        Set<String> set = sourceMap.keySet();
+
+        for (String str : set) {
+            Integer key = str.length();
+            Set<String> buff;
+
+            if (map.containsKey(key)) buff = map.get(key);
+            else buff = new HashSet<>();
+
+            buff.add(str);
+            map.put(key, buff);
+        }
+
+        return map;
     }
 }
